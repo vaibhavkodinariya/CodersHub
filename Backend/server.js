8692815dc28157452fe2f04bc05aaee7159def78
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2024-02-22 19:45:43 +0530

    server Structure Done

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/db");
+const port = process.env.PORT;
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+connectDB();
+app.use(express.urlencoded({ extended: false }));
+app.listen(port, () => {
+  console.log("Server Started and Port Number", port);
+});
