7328fe48ebdd148fb2d85f0904af57aef056ae22
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2024-02-27 18:50:49 +0530

    Login Logic Done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,18 @@
 const asyncHandler = require("express-async-handler");
+const { User } = require("../models/userModel");
+const bcry = require("bcrypt");
 
-const Login = asyncHandler(async (req, res) => {});
+//  @access Private
+//  @desc Login Method
+//  @path cpp/user/login
+const Login = asyncHandler(async (req, res) => {
+  const { email, type, password } = req.body;
+  if (!email || !type || !password)
+    return res.send(400).json({ message: "Please provide all the details" });
+
+  let user = await User.findOne({ email });
+  if (user && (await bcry.compare(password, user.password))) {
+  } else return res.send(400).json({ message: "Incorrect Email or Password" });
+});
 
 module.exports = { Login };
