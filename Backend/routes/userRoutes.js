530675bd6ac6b97f2cdb98e0d56a2bdba26db7ec
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2024-02-29 19:01:02 +0530

    register Routes Added

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,10 +3,12 @@ const {
   login,
   logout,
   refreshAccessToken,
+  register,
 } = require("../controllers/userController");
 const { verifyJWT } = require("../middleware/authMiddleware");
 const router = express.Router();
 
+router.post("/", register);
 router.post("/login", login);
 router.post("/logout", verifyJWT, logout);
 router.post("/refresh", refreshAccessToken);
